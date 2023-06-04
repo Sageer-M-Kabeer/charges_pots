4343bf5b7b76361ac2888f90eafd5425b8bf0439
Sageer-m-Kabeer <sageerm379@gmail.com>
2023-06-04 16:55:26 +0000

    added fonts amd made changes in mine.js

--- chargespots/src/pages/Mine.js
+++ chargespots/src/pages/Mine.js
@@ -7,7 +7,9 @@ import {TbReportMoney,TbReport} from 'react-icons/tb'
 import {RiCustomerService2Line} from 'react-icons/ri'
 import {MdOutlineLanguage} from 'react-icons/md'
 import {GoSignOut} from 'react-icons/go'
+import {VscRefresh} from 'react-icons/vsc'
 import { Link } from 'react-router-dom'
+
 
 
 const Mine = () => {
@@ -20,26 +22,22 @@ const Mine = () => {
                 {/* card */}
                 <div className="relative  mb-8 bg-[#fff]  shadow-sm rounded-2xl">
                     <div className="p-8">
-                        <div className="flex p-2">
-                            <div className=" w-14 h-14 ">
+                        <div className="flex p-1">
+                            <div className=" w-12 h-12 ">
                                 <img className="w-full h-full" src={logo} alt="" />
                             </div>
                             <div className="flex-1 pl-3 flex-col justify-center">
-                                <div className="mb-[10px]text-md font-medium flex-1 text-[#333] gap-10  flex-row w-full overflow-hidden justify-between items-center">
-                                    <div>
-                                        8147354770
+                                <div className="flex mb-8 justify-between h-6 leading-[24px] font-medium">
+                                <div>8147354770</div>
                                         <div>
-                                        <button type="button" className="text-white bg-[rgb(24,149,176)] h-[24px] p-2 text-center flex justify-center items-center text-[10px] rounded-md ">
-                                            refresh
+                                        <button type="button" className="text-white right-0 bg-[rgb(24,149,176)] h-[24px] p-2 text-center flex justify-center items-center text-lg font-bold rounded-md ">
+                                            <VscRefresh/>
                                         </button>
                                     </div>
-                                    </div>
-                                    
-
                                 </div>
                                 <div className="flex justify-between h-6 leading-[24px] font-medium">
                                     <div>invitation code</div>
-                                    <div className="text-[#1895b0] p-[4px,8px] w-[40%] rounded-md text-center bg-[rgb(24,149,176,.1)] ">
+                                    <div className="text-[#1895b0] p-[4px,8px] w-20 rounded-md text-center bg-[rgb(24,149,176,.1)] ">
                                     FJDYJS
                                 </div>
                                 </div>
@@ -78,72 +76,72 @@ const Mine = () => {
                     <div className="p-8 flex justify-between flex-col gap-4">
                         <Link className="m-[0,16px] pt-3 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                                <div className='text-2xl items-center mr-2'> <TbReportMoney/> </div>
-                                <div className="flex-1 "> <span className="text-lg">Recharge Record</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                                <div className='text-lg items-center mr-2'> <TbReportMoney/> </div>
+                                <div className="flex-1 "> <span className="text-sm">Recharge Record</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
                             <hr className="w- text-gray-100 h-1 mt-2"/>
 
                         </Link>
-                        <Link className="m-[0,16px] pt-2 overflow-hidden bg-white">
+                        <Link className="m-[0,16px] pt-1 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                                <div className='text-2xl items-center mr-2'> <TbReportMoney/> </div>
-                                <div className="flex-1 "> <span className="text-lg">Withdrawal Record</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                                <div className='text-lg items-center mr-2'> <TbReportMoney/> </div>
+                                <div className="flex-1 "> <span className="text-sm">Withdrawal Record</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
 
                         </Link>
-                        <hr className="w- text-gray-100 h-1 mt-2"/>
+                        <hr className="w- text-gray-100 h-1 mt-1"/>
 
-                        <Link className="m-[0,16px] pt-2 overflow-hidden bg-white">
+                        <Link className="m-[0,16px] pt-1 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                                <div className='text-2xl items-center mr-2'> <TbReport/> </div>
-                                <div className="flex-1 "><span className="text-lg">Check in Record</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                                <div className='text-lg items-center mr-2'> <TbReport/> </div>
+                                <div className="flex-1 "><span className="text-sm">Check in Record</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
 
                         </Link>
-                        <hr className="w- text-gray-100 h-1 mt-2"/>
+                        <hr className="w- text-gray-100 h-1 mt-1"/>
 
-                        <Link className="m-[0,16px] pt-2 overflow-hidden bg-white">
+                        <Link className="m-[0,16px] pt-1 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                            <div className='text-2xl items-center mr-2'> <AiOutlineCreditCard/> </div>
-                                <div className="flex-1 "> <span className="text-lg">Blind Bank Card</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                            <div className='text-lg pt-[2px] items-center mr-2'> <AiOutlineCreditCard/> </div>
+                                <div className="flex-1 "> <span className="text-sm">Blind Bank Card</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
 
                         </Link>
-                        <hr className="w- text-gray-100 h-1 mt-2"/>
+                        <hr className="w- text-gray-100 h-1 mt-1"/>
 
-                        <Link className="m-[0,16px] pt-2 overflow-hidden bg-white">
+                        <Link className="m-[0,16px] pt-1 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                                <div className='text-2xl items-center mr-2'> <RiCustomerService2Line/> </div>
-                                <div className="flex-1 "> <span className="text-lg">Customer Service</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                                <div className='text-lg items-center mr-2'> <RiCustomerService2Line/> </div>
+                                <div className="flex-1 "> <span className="text-sm">Customer Service</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
 
                         </Link>
-                        <hr className="w- text-gray-100 h-1 mt-2"/>
+                        <hr className="w- text-gray-100 h-1 mt-1"/>
 
                         <Link className="m-[0,16px] pt-2 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                            <div className='text-2xl items-center mr-2'> <MdOutlineLanguage/> </div>
-                                <div className="flex-1 "> <span className="text-lg">Modern Language</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                            <div className='text-lg pt-[2px] items-center mr-2'> <MdOutlineLanguage/> </div>
+                                <div className="flex-1 "> <span className="text-sm">Modern Language</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
 
                         </Link>
-                        <hr className="w- text-gray-100 h-1 mt-2"/>
+                        <hr className="w- text-gray-100 h-1 mt-1"/>
 
-                        <Link className="m-[0,16px] pt-2 overflow-hidden bg-white">
+                        <Link className="m-[0,16px] pt-1 overflow-hidden bg-white">
                             <div className="flex relative box-border w-full p-[10px,16px] text-[#323232] text-[14px] leading-[24px]">
-                            <div className='text-2xl items-center mr-2'> <GoSignOut/> </div>
-                                <div className="flex-1 "> <span className="text-lg">Sign Out</span></div>
-                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-lg"><AiOutlineRight/></span></div>
+                            <div className='text-lg pt-1 items-center mr-2'> <GoSignOut/> </div>
+                                <div className="flex-1 "> <span className="text-sm">Sign Out</span></div>
+                                <div className="relative text-right text-[#969799] overflow-hidden "> <span className="text-sm"><AiOutlineRight/></span></div>
                             </div>
                         </Link>
-                        <hr className="w- text-gray-100 h-1 mt-2"/>
-                        
+                        <hr className="w- text-gray-100 h-1 mt-1"/>
+
                     </div>
                 </div>
             </div>
